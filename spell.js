412045585bf2c7444e2d65402cfe76111e6542ed
Viktoria <vaku0004@algonquinlive.com>
2025-05-09 22:28:28 -0400

    adds new page

---
+++ spell.js
@@ -0,0 +1,12 @@
+const wordPairs = [
+  { distorted: 'expen_es', correct: 'expenses' },
+  { distorted: 'ne_e_ary', correct: 'necessary' },
+  { distorted: 'techni__s', correct: 'techniques' },
+  { distorted: 'decaffe__', correct: 'decaffeinated' },
+  { distorted: '_mplo_e', correct: 'employee' },
+  { distorted: 'su_e_ful', correct: 'successful' },
+  { distorted: 'len__', correct: 'length' },
+  { distorted: 'thou__', correct: 'thought' },
+  { distorted: 'cons_t_ntly', correct: 'consistently' },
+  { distorted: 'tau__', correct: 'taught' }
+];
